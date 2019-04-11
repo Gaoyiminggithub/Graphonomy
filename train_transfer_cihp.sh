505b9e4499cf21d0f987fc217fc3178f54c9ac58
gaoyi <617379377@qq.com>
2019-04-11 22:47:37 +0800

    init

--- train_transfer_cihp.sh
+++ train_transfer_cihp.sh
@@ -1,2 +1,3 @@
 python ./exp/transfer/train_cihp_from_pascal.py \
- --batch 24 --gpus 8 --pretrainedModel './pascal_base_trained.pth'
+ --batch 24 --gpus 8 --pretrainedModel './pascal_base_trained.pth' \
+ --lr 0.007 --classes 20
--- train_universal.sh
+++ train_universal.sh
@@ -1,3 +1,4 @@
 python ./exp/universal/pascal_atr_cihp_uni.py \
  --batch 24 --gpus 8 \
- --pretrainedModel './data/pretrained_model/deeplab_v3plus_v3.pth'
+ --pretrainedModel './data/pretrained_model/deeplab_v3plus_v3.pth' \
+ --lr 0.007
