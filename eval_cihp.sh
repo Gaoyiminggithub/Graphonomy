7cdafc49bc48bc84af489d34581fd4eaa0e0a693
gaoyi <617379377@qq.com>
2019-04-12 19:47:05 +0800

    init

--- eval_cihp.sh
+++ eval_cihp.sh
@@ -1,6 +1,5 @@
 python ./exp/test/eval_show_pascal2cihp.py \
  --batch 1 --gpus 1 --classes 20 \
- --gt_path './data/datasets/CIHP_4w/Category_ids/' \
- --txt_file './data/datasets/CIHP_4w/lists/test_id.txt' \
+ --gt_path './data/datasets/CIHP_4w/Category_ids' \
+ --txt_file './data/datasets/CIHP_4w/lists/val_id.txt' \
  --loadmodel './data/pretrained_model/inference.pth'
-
--- eval_pascal.sh
+++ eval_pascal.sh
@@ -1,6 +1,5 @@
-python ./exp/test/eval_show_cihp2pascal.py \
- --batch 1 --gpus 1 --classes 20 \
- --gt_path './data/datasets/CIHP_4w/Category_ids/' \
- --txt_file './data/datasets/CIHP_4w/lists/test_id.txt' \
- --loadmodel './data/pretrained_model/cihp2pascal.pth'
-
+python ./exp/test/eval_show_pascal.py \
+ --batch 1 --gpus 1 --classes 7 \
+ --gt_path './data/datasets/pascal/SegmentationPart/' \
+ --txt_file './data/datasets/pascal/list/val_id.txt' \
+ --loadmodel './cihp2pascal.pth'
